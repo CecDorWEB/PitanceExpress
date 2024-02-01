f72bcc49cf0b4e45a3ec2d28cc7db5cae77e07a3
Margot <margot.voydis@gmail.com>
2024-02-01 14:44:34 +0100

    add text to home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,18 @@
-import "../Styles/home.css"
+import "../Styles/home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Pitance Express</h1>
+      <p>
+        Oyé Oyé ! Soyez les bienvenues sur les pages électroniques de Pitance
+        Express, où vous pouvez jouir des délices médiévaux, délivrés
+        promptement jusqu&aposà votre demeure par notre fidèle charette,
+        assurant ainsi une expérience gastronomique sans pareille.
+      </p>
+      <button>Recevoir la délivrance par l&aposentremise d&aposun gueu</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
