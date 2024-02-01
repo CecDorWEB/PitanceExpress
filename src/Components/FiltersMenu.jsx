03488a29508d1fd99b06a35dbca4216e7fce5c77
Axellesti <sartori.axelle18@gmail.com>
2024-02-01 23:10:42 +0100

    US11_almost_done

--- src/Components/FiltersMenu.jsx
+++ src/Components/FiltersMenu.jsx
@@ -1,5 +1,44 @@
+import "../Styles/menu.css";
+import { useContext, useState } from "react";
+import FavoriteContext from "../Context/FavoriteContext";
+import medievalRecipes from "../medievalRecipes";
+
 function FiltersMenu() {
-  return <div>FiltersMenu</div>;
+  const recipe = medievalRecipes;
+  const [activeCategory, setActiveCategory] = useState("");
+
+  const { filterValue, setFilterValue } = useContext(FavoriteContext);
+
+  const filterMenu = (value) => {
+    setFilterValue(value);
+    setActiveCategory(value);
+  };
+  console.info(filterValue);
+
+  return (
+    <section className="filters_container">
+      <div className="buttons_menu">
+        <button
+          className={activeCategory === "main course" ? "active" : ""}
+          onClick={() => filterMenu("main course")}
+        >
+          Mets
+        </button>
+        <button
+          className={activeCategory === "drink" ? "active" : ""}
+          onClick={() => filterMenu("drink")}
+        >
+          Breuvages
+        </button>
+        <button
+          className={activeCategory === "dessert" ? "active" : ""}
+          onClick={() => filterMenu("dessert")}
+        >
+          Douceurs
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default FiltersMenu;
--- src/Context/FavoriteContext.jsx
+++ src/Context/FavoriteContext.jsx
@@ -6,11 +6,22 @@ const FavoriteContext = createContext();
 export function FavoriteProvider({ children }) {
   const [favorite, setFavorite] = useState([]);
   const [quantityTotal, setQuantityTotal] = useState(0);
+  const [filterValue, setFilterValue] = useState("");
+  const [activeCategory, setActiveCategory] = useState("");
 
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
     <FavoriteContext.Provider
-      value={{ favorite, setFavorite, quantityTotal, setQuantityTotal }}
+      value={{
+        favorite,
+        setFavorite,
+        quantityTotal,
+        setQuantityTotal,
+        filterValue,
+        setFilterValue,
+        activeCategory,
+        setActiveCategory,
+      }}
     >
       {children}
     </FavoriteContext.Provider>
