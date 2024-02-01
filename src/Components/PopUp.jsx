643ca8240028c8deabb789847bc1b1ccbefb8b47
yohan59235 <yohan.collinse@gmail.com>
2024-02-01 20:18:56 +0100

    create popup

---
+++ src/Components/PopUp.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/no-unescaped-entities */
+import PropTypes from "prop-types";
+
+import "../Styles/popup.css";
+
+function PopUp({ trigger, setTrigger }) {
+  return trigger ? (
+    <div className="popup">
+      <div className="popup_inner">
+        <button
+          type="button"
+          className="close_btn"
+          onClick={() => setTrigger(false)}
+        >
+          X
+        </button>
+        <div className="Desc_popup">
+          <p>
+            Merci pour votre commande, votre victuaille est en route, préparez
+            vos plus belles écuelles pour accueillir les mets, vos cruches pour
+            le breuvage et attablez vous prêt à faire ripaille !
+          </p>
+        </div>
+        <div className="Service">
+          <h5>Prit en charge part :</h5>
+          <h6>Abdou et Kévin</h6>
+        </div>
+        <div className="Time_Desserve">
+          <p>Temps de livraison : 35min</p>
+          <p>
+            Désolé messire mais nous n'avons pas choisi les meilleurs livreurs,
+            ils sont en carriole et dotés de deux mains gauches
+          </p>
+        </div>
+      </div>
+    </div>
+  ) : null;
+}
+
+export default PopUp;
+
+PopUp.propTypes = {
+  trigger: PropTypes.bool.isRequired,
+  setTrigger: PropTypes.func.isRequired,
+};
