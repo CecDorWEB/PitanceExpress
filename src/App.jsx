84c40c535c45cb2590e5f559edcc07622cc5bd0f
CECILE DORANGE <cecile.dorange45@gmail.com>
2024-02-01 15:13:51 +0100

    US13

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,9 @@ function App() {
   console.info(recipe);
   return (
     <>
-      <h1>Pitance Express</h1>
-      <h1>{recipe[0].name}</h1>
-      <img src={recipe[0].img}></img>
-      <p>Categrory:{recipe[0].category}</p>
-      <p>Allergen: {recipe[0].allergen}</p>
-    <main>
-      <Outlet />
-    </main>
-    
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
