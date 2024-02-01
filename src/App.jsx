ea33ee904095dcfe9532347ebbbc928a6e8ec67d
yohan59235 <yohan.collinse@gmail.com>
2024-02-01 16:11:32 +0100

    create page cash

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import { Outlet } from "react-router-dom";
 
 import "./App.css";
-import medievalRecipes from "./medievalRecipes";
 
 function App() {
-  const recipe = medievalRecipes;
-  console.info(recipe);
   return (
     <>
-      <h1>Pitance Express</h1>
-      <h1>{recipe[0].name}</h1>
-      <img src={recipe[0].img}></img>
-      <p>Categrory:{recipe[0].category}</p>
-      <p>Allergen: {recipe[0].allergen}</p>
     <main>
       <Outlet />
     </main>
-    
     </>
   );
 }
